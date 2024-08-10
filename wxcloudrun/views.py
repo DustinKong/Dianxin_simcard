faedc364e79e1f96d5aeb4b4091954fcd20a6ecb
Dustin Kong <631410882@qq.com>
2024-08-10 14:55:14 +0800

    add school b post

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -56,6 +56,48 @@ def count():
     else:
         return make_err_response('action参数错误')
 
+@app.route('/api/count_school_b', methods=['POST'])
+def count_b():
+    """
+    :return:计数结果/清除结果
+    """
+
+    # 获取请求体参数
+    params = request.get_json()
+
+    # 检查action参数
+    if 'action' not in params:
+        return make_err_response('缺少action参数')
+
+    # 按照不同的action的值，进行不同的操作
+    action = params['action']
+
+    # 执行自增操作
+    if action == 'inc':
+        counter = query_counterbyid(1)
+        if counter is None:
+            counter = Counters()
+            counter.id = 2
+            counter.count = 1
+            counter.created_at = datetime.now()
+            counter.updated_at = datetime.now()
+            insert_counter(counter)
+        else:
+            counter.id = 2
+            counter.count += 1
+            counter.updated_at = datetime.now()
+            update_counterbyid(counter)
+        return make_succ_response(counter.count)
+
+    # 执行清0操作
+    elif action == 'clear':
+        delete_counterbyid(2)
+        return make_succ_empty_response()
+
+    # action参数错误
+    else:
+        return make_err_response('action参数错误')
+
 
 @app.route('/api/count', methods=['GET'])
 def get_count():
@@ -63,4 +105,12 @@ def get_count():
     :return: 计数的值
     """
     counter = Counters.query.filter(Counters.id == 1).first()
+    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+@app.route('/api/count_school_b', methods=['GET'])
+def get_count():
+    """
+    :return: 计数的值
+    """
+    counter = Counters.query.filter(Counters.id == 2).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
